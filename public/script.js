759ef68876db51f782eead2b1776f3523bac15d4
Yazeedz91 <yazeedz91@hotmail.com>
2021-04-09 21:09:48 -0400

    W10 - added table back

--- public/script.js
+++ public/script.js
@@ -1,4 +1,34 @@
 window.onload = async function () {
+    
+    // Create Table
+
+    const table = document.querySelector('#dining_halls');
+    
+    const request = await fetch('/api/dining');
+    const data = await request.json();
+    const hall_array = data.data;
+
+    console.log(hall_array);
+
+    hall_array.forEach((element) => {
+        const hall_name = element.hall_name;
+        const hall_loc = element.hall_address;
+        const table_row = document.createElement('tr');
+        table_row.classList.add('tr');
+        table_row.innerHTML = `
+                            <td class="td">${hall_name}</td>
+                            <td class="td">${hall_loc}</td>
+                        `;
+        table.append(table_row);
+    });
+
+    Object.keys(data).forEach((element) => {
+        Object.keys(element).forEach((hall) => {
+        
+        });
+    });
+
+    // Create Chart
 
     const req_meals = await fetch('/api/meals')
     const req_macros = await fetch('/api/macros')
